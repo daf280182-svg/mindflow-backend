a4626ab65de4f50f1f5026541714f685b7abe832
daniloconoventa <daf280182@gmail.com>
2026-04-13 22:48:45 -0300

    fix backend cors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require("express");
+const express = 
+require("express");
 const cors = require("cors");
-app.use(cors());
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 
