974ef2473be4abb1b8b981bfe595d48d5ed1b7ee
daniloconoventa <daf280182@gmail.com>
2026-04-15 14:42:36 -0300

    fix port render

--- server.js
+++ server.js
@@ -44,6 +44,5 @@ app.get("/historial", (req, res)
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Servidor 
-corriendo en puerto " + PORT);
+  console.log("Servidor corriendo en puerto " + PORT);
 });
